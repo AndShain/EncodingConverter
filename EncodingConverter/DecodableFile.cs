12be09a975e10d6e137adf03cd7b0e262c045f97
Andrey <a.a.sh.6556@gmail.com>
2022-08-11 14:36:08 +0600

    Added missing classes

---
+++ EncodingConverter/DecodableFile.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace EncodingConverter
+{
+    public class DecodableFile
+    {
+        // Кодировка файла
+        public Encoding Encoding { get; set; } = Encoding.Default;
+
+        // Текст файла в текущей кодировке
+        public string Text { get; set; } = string.Empty;
+
+        // Флаг, который показывает, менялась кодировка или нет
+        public bool EncodingСhanged { get; set; } = false;
+
+        public DecodableFile(Encoding encoding, string text)
+        {
+            Encoding = encoding;
+            Text = text;
+        }
+    }
+}
---
+++ EncodingConverter/Logger.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Text;
+
+namespace EncodingConverter
+{
+    public class Logger
+    {
+        // Название файла для логирования
+        private static string logFileName = "Logs.txt";
+
+        // Записывает текст в файл для логирования
+        public static void WriteTextToLog(string text)
+        {
+            using (StreamWriter sw = new StreamWriter(logFileName, true, Encoding.UTF8))
+            {
+                sw.Write(text);
+            }
+        }
+    }
+}
